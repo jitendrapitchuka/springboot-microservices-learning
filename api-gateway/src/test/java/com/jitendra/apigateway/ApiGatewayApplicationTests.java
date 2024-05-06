0f65ec3a6e222b76aa1f32ea0f50e1d4a9ad515f
jitendra <jithendrapitchuka@gmail.com>
2024-05-06 14:31:52 +0530

    imple apigateway and swagger Ui

---
+++ api-gateway/src/test/java/com/jitendra/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jitendra.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
